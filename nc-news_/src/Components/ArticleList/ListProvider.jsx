0d34dc71e8d6fdaf3411ca0479d2c27417a014da
AlexanderThePrettyDecent <xander.waring@outlook.com>
2024-11-06 09:49:49 +0000

    consolidated article body and single article files into a single file to facilitate the addition of loading states

--- nc-news_/src/Components/ArticleList/ListProvider.jsx
+++ nc-news_/src/Components/ArticleList/ListProvider.jsx
@@ -2,12 +2,14 @@ import apiClient from "../../api";
 import { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import "./articleList.css";
+import RingLoader from "react-spinners/RingLoader";
 
 const ListProvider = () => {
   const [articleList, setArticleList] = useState([]);
   const [page, setPage] = useState(1);
   const [articleCount, setArticleCount] = useState(0);
   const totalPages = Math.ceil(articleCount / 10);
+  const [loading, setLoading] = useState(true);
 
   const nextPageHandler = () => {
     setPage(page + 1);
@@ -22,6 +24,7 @@ const ListProvider = () => {
       .then((response) => {
         setArticleList(response.data.articles);
         setArticleCount(response.data.total_count);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -30,11 +33,15 @@ const ListProvider = () => {
 
   return (
     <div id="listBack">
-      <ul id="list">
-        {articleList.map((article) => {
-          return <ArticleCard article={article} />;
-        })}
-      </ul>
+      {loading ? (
+        <RingLoader id="loader" />
+      ) : (
+        <ul id="list">
+          {articleList.map((article) => {
+            return <ArticleCard article={article} />;
+          })}
+        </ul>
+      )}
       <div id="buttonZone">
         <button
           disabled={!(page > 1)}
--- nc-news_/src/Components/SingleArticle/SingleArticle.jsx
+++ nc-news_/src/Components/SingleArticle/SingleArticle.jsx
@@ -1,16 +1,107 @@
-import { useParams } from "react-router";
-import ArticleBody from "./ArticleBody";
-import CommentBox from "./CommentBox";
+import apiClient from "../../api";
+import { useEffect, useState } from "react";
+import RingLoader from "react-spinners/RingLoader";
 import "./article.css";
+import CommentBox from "./CommentBox";
+import { useParams } from "react-router";
 
 const SingleArticle = () => {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [article, setArticle] = useState({});
+  const [voteCount, setVoteCount] = useState(0);
+  const [voted, setVoted] = useState("none");
+
+  useEffect(() => {
+    apiClient
+      .get(`articles/${id}`)
+      .then((response) => {
+        setVoteCount(response.data.article.votes);
+
+        setLoading(false);
+        setArticle(response.data.article);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const upVoteHandler = () => {
+    if (voted === "none") {
+      setVoted("up");
+      setVoteCount(voteCount + 1);
+      apiClient.patch(`/articles/${id}`, { inc_votes: 1 }).catch((err) => {
+        setVoted("none");
+        setVoteCount(voteCount);
+      });
+    } else {
+      setVoted("none");
+      setVoteCount(voteCount - 1);
+      apiClient.patch(`/articles/${id}`, { inc_votes: -1 }).catch((err) => {
+        setVoted("up");
+        setVoteCount(voteCount);
+      });
+    }
+  };
+  const downVoteHandler = () => {
+    if (voted === "none") {
+      setVoted("down");
+      setVoteCount(voteCount - 1);
+      apiClient.patch(`/articles/${id}`, { inc_votes: -1 }).catch((err) => {
+        setVoted("none");
+        setVoteCount(voteCount);
+      });
+    } else {
+      setVoted("none");
+      setVoteCount(voteCount + 1);
+      apiClient.patch(`/articles/${id}`, { inc_votes: +1 }).catch((err) => {
+        setVoted("down");
+        setVoteCount(voteCount);
+      });
+    }
+  };
 
   return (
-    <div  id="articleWhole">
-      <ArticleBody id={id} />
-      <CommentBox id={id} />
-    </div>
+    <>
+      {loading === false ? (
+        <div id="articleWhole">
+          <div id="bodyBack">
+            <h1 id="articleTitle">{article.title}</h1>
+            <h3 id="details">
+              By {article.author} {new Date(article.created_at).getDate()}/
+              {new Date(article.created_at).getMonth()}/
+              {new Date(article.created_at).getFullYear()}
+            </h3>
+            <div id="imageBox">
+              <img src={article.article_img_url} id="image"></img>
+            </div>
+            <p id="articleBody">{article.body}</p>
+            <div id="voteBox">
+              <button
+                className="button"
+                id="upVote"
+                disabled={voted === "down"}
+                onClick={upVoteHandler}
+              >
+                +1
+              </button>
+              <p id="voteCount">Votes: {voteCount}</p>
+              <button
+                className="button"
+                id="downVote"
+                disabled={voted === "up"}
+                onClick={downVoteHandler}
+              >
+                -1
+              </button>
+            </div>
+          </div>
+          <CommentBox id={id} />
+        </div>
+      ) : (
+        <RingLoader id="loader" />
+      )}
+    </>
   );
 };
 
